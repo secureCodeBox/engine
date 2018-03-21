3e0e4a8d5cee755c6b5a0f9b68b44cf06e1330a4
Michael Peyerl <michael.peyerl@iteratec.at>
2018-03-21 09:39:51 +0100

    First Tests for Nmap Process

--- scb-scanprocesses/nmap-process/src/test/java/NmapProcessTest.java
+++ scb-scanprocesses/nmap-process/src/test/java/NmapProcessTest.java
@@ -1,9 +1,32 @@
+import io.securecodebox.constants.DefaultFields;
+import io.securecodebox.scanprocess.NmapScanProcessExecution;
+import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.ProcessEngineException;
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.camunda.bpm.engine.test.Deployment;
 import org.camunda.bpm.engine.test.ProcessEngineRule;
+import org.camunda.bpm.extension.mockito.mock.FluentExecutionListenerMock;
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.camunda.bpm.engine.test.assertions.bpmn.AbstractAssertions.processEngine;
+import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareAssertions.assertThat;
+import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.execute;
+import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.job;
+import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.runtimeService;
+import static org.camunda.bpm.extension.mockito.CamundaMockito.autoMock;
+import static org.camunda.bpm.extension.mockito.CamundaMockito.registerExecutionListenerMock;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @Deployment(resources = "bpmn/nmap_scan.bpmn")
@@ -13,14 +36,67 @@ public class NmapProcessTest {
     private static final String DO_PORTSCAN_TASK_ID = "ServiceTask_DoPortscan";
     private static final String TRANSFORM_RESULTS_TASK_ID = "ServiceTask_TransformResult";
 
+    private final Map<String, Object> defaultVariables = new HashMap<>();
+
     @Rule
     public ProcessEngineRule processEngineRule = new ProcessEngineRule();
+
+    @Before
+    public void init(){
+        defaultVariables.put(DefaultFields.PROCESS_AUTOMATED.name(), true);
+    }
 
     @Test
     public void testDeployment(){}
 
     @Test
+    public void testAutomatedStart_shouldPass(){
+        ProcessInstance processInstance = runtimeService().startProcessInstanceByKey(PROCESS_ID, defaultVariables);
+        assertThat(processInstance).isStarted();
+    }
+
+    @Test(expected = ProcessEngineException.class)
+    public void testStartWithoutVariables_shouldFail(){
+        runtimeService().startProcessInstanceByKey(PROCESS_ID);
+    }
+
+    @Test(expected = ProcessEngineException.class)
+    public void testManualStartWithoutConfiguration_shouldFail(){
+        Map<String, Object> variables = new HashMap<>();
+        variables.put(DefaultFields.PROCESS_AUTOMATED.name(), false);
+        runtimeService().startProcessInstanceByKey(PROCESS_ID, variables);
+    }
+
+    @Test
+    public void testManualStartWithConfiguration_shouldPass(){
+        Map<String, Object> variables = new HashMap<>();
+        variables.put(DefaultFields.PROCESS_AUTOMATED.name(), false);
+        //todo: comment this in, when NmapFields exist
+//        variables.put(NmapFields.CONFIGURATION_TYPE.name(), "default");
+        ProcessInstance processInstance = runtimeService().startProcessInstanceByKey(PROCESS_ID, variables);
+        assertThat(processInstance).isStarted(); }
+
+
+
+    @Test
     public void testScanProcess(){
 
+        autoMock("bpmn/nmap_scan.bpmn");
+        Map<String, Object> variables = new HashMap<>();
+        variables.put(DefaultFields.PROCESS_AUTOMATED.name(), true);
+        ProcessInstance processInstance = processEngine().getRuntimeService().startProcessInstanceByKey(PROCESS_ID, variables);
+
+        assertThat(processInstance).isStarted();
+        //Make sure that the next task to execute is "Run Portscan"
+        assertEquals(Collections.singletonList(DO_PORTSCAN_TASK_ID), runtimeService().getActiveActivityIds(processInstance.getId()));
+
+        //Execute the next job
+        execute(job());
+//
+//        //Make sure the next task is the "Transform Generic Results"
+//        assertEquals(Collections.singletonList(TRANSFORM_RESULTS_TASK_ID), runtimeService().getActiveActivityIds(processInstance.getId()));
+
     }
+
+
 }
