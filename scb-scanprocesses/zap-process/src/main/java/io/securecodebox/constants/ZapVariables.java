747b29807dd10278bafa67aa331806184d393e55
Michael Peyerl <michael.peyerl@iteratec.at>
2018-04-10 15:28:20 +0200

    add zap process to engine

---
+++ scb-scanprocesses/zap-process/src/main/java/io/securecodebox/constants/ZapVariables.java
@@ -0,0 +1,25 @@
+package io.securecodebox.constants;
+
+@Deprecated
+public class ZapVariables {
+
+	public static final String ZAP_AUTH_CONFIGURATION_TYPE = "authentication";
+	public static final String ZAP_SPIDER_CONFIGURATION_TYPE = "spiderConfigurationType";
+	public static final String ZAP_SPIDER_RESULT_RAW = "spiderResult";
+	public static final String ZAP_SPIDER_RESULT_JSON = "spiderResult_json";
+	public static final String ZAP_SPIDER_MICROSERVICE_ID = "spiderMicroserviceId";
+	public static final String ZAP_SPIDER_TARGET = "spiderTargetUrl";
+	public static final String ZAP_SPIDER_TYP = "spiderType";
+	public static final String ZAP_SCANNER_CONFIGURATION_TYPE = "scannerConfigurationType";
+	public static final String ZAP_SCANNER_RESULT_RAW = "scannerResult";
+	public static final String ZAP_SCANNER_RESULT_JSON = "scannerResult_json";
+	public static final String ZAP_SCANNER_MICROSERVICE_ID = "scannerMicroserviceId";
+	public static final String ZAP_SCANNER_TARGET = "scannerTargetUrl";
+	public static final String ZAP_SCANNER_TYP = "scannerType";
+	public static final String ZAP_REPORTING_CONFIGURATION_TYPE = "reportingConfigurationType";
+	public static final String ZAP_REPORTING_RESULT_RAW = "reportingResult";
+	public static final String ZAP_REPORTING_RESULT_JSON = "reportingResult_json";
+	public static final String ZAP_REPORTING_TYPES = "reportingTypes";
+	public static final String ZAP_REPORTING_FALSEPOSITVES = "reportingFalsePositives";
+	public static final String ZAP_REPORTING_PARSER = "reportingParser";
+}
