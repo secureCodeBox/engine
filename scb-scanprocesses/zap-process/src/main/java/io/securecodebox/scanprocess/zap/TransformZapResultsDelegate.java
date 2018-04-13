c953bfb5cc3718010eef245fee7ec1ca616a48a4
Michael Peyerl <michael.peyerl@iteratec.at>
2018-04-13 14:19:43 +0200

    add listeners for zap

--- scb-scanprocesses/zap-process/src/main/java/io/securecodebox/scanprocess/zap/TransformZapResultsDelegate.java
+++ scb-scanprocesses/zap-process/src/main/java/io/securecodebox/scanprocess/zap/TransformZapResultsDelegate.java
@@ -4,10 +4,12 @@ import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 /**
  * This delegate transforms the raw ZAP Results into the generic findings
  */
+@Component
 public class TransformZapResultsDelegate  implements JavaDelegate {
 
     private static final Logger LOG = LoggerFactory.getLogger(TransformZapResultsDelegate.class);
