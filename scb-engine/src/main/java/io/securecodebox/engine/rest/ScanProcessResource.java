fd8d8151516f7d4e6b7c2c0aadc54826fded01f4
Rüdiger Heins <ruediger.heins@iteratec.de>
2018-04-27 15:34:07 +0200

    implements first scan process resource

--- scb-engine/src/main/java/io/securecodebox/engine/rest/ScanProcessResource.java
+++ scb-engine/src/main/java/io/securecodebox/engine/rest/ScanProcessResource.java
@@ -19,13 +19,19 @@
 
 package io.securecodebox.engine.rest;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import io.securecodebox.constants.DefaultFields;
 import io.securecodebox.model.execution.Target;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.repository.ProcessDefinition;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -34,7 +40,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * API / Endpoint for scan processes (camunda processes).
@@ -49,36 +58,78 @@ import java.util.List;
 public class ScanProcessResource {
     private static final Logger LOG = LoggerFactory.getLogger(ScanProcessResource.class);
 
+    @Autowired
+    ProcessEngine engine;
+
     @ApiOperation(value = "Creates a new scan process. (API STUB NOT IMPLEMENTED!)")
     @ApiResponses(
             value = { @ApiResponse(code = 201, message = "Successful delivery of the failure.", response = void.class),
+                    @ApiResponse(code = 300,
+                            message = "For some reason multiple processes could be adressed by the given processKey.",
+                            response = void.class),
                     @ApiResponse(code = 400, message = "Incomplete or inconsistent Request"),
                     @ApiResponse(code = 500, message = "Unknown technical error occurred.") })
 
-    @RequestMapping(method = RequestMethod.PUT, value = "/{processName}")
-    public ResponseEntity createProcess(@PathVariable String processName, @RequestBody List<Target> targets) {
+    @RequestMapping(method = RequestMethod.PUT, value = "/{processKey}")
+    public ResponseEntity getProcesses(@PathVariable String processKey, @RequestBody List<Target> targets) {
 
-        LOG.debug("Recived scan failure {}", targets);
+        long processCount = engine.getRepositoryService()
+                .createProcessDefinitionQuery()
+                .active()
+                .processDefinitionKey(processKey)
+                .latestVersion()
+                .count();
 
-        //        Map<String, Object> variables = new HashMap<>();
-        //        variables.put(DefaultFields.PROCESS_SCANNER_ID.name(), targets.getScannerId().toString());
-        //        variables.put(DefaultFields.PROCESS_SCANNER_TYPE.name(), targets.getScannerType());
-        //        variables.put(DefaultFields.PROCESS_RAW_FINDINGS.name(), targets.getRawFindings());
-        //        synchronized (DefaultFields.PROCESS_FINDINGS) {
-        //            try {
-        //                ObjectValue objectValue = Variables.objectValue(objectMapper.writeValueAsString(targets.getFindings()))
-        //                        .serializationDataFormat(Variables.SerializationDataFormats.JSON)
-        //                        .create();
-        //                variables.put(DefaultFields.PROCESS_FINDINGS.name(), objectValue);
-        //            } catch (JsonProcessingException e) {
-        //                LOG.error("Can't write field {} to process!", DefaultFields.PROCESS_FINDINGS, e);
-        //                throw new IllegalStateException("Can't write field to process!", e);
-        //            }
-        //        }
-        //
-        //        engine.getExternalTaskService().complete(id.toString(), targets.getScannerId().toString(), variables);
+        Map<String, Object> values = new HashMap<>();
+        values.put(DefaultFields.PROCESS_AUTOMATED.name(), true);
 
-        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).build();
+        if (processCount == 1) {
+            LOG.debug("Starting process for id {}", processKey);
+            engine.getRuntimeService().startProcessInstanceByKey(processKey, values);
+            return ResponseEntity.ok().build();
+        } else {
+            LOG.error("Found more than one result for processKey: {}", processKey);
+            return ResponseEntity.status(HttpStatus.MULTIPLE_CHOICES).build();
+        }
+
+    }
+
+    @ApiOperation(value = "Returns all possible processes. (API STUB NOT IMPLEMENTED!)")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successful delivery of the failure.", response = Result.class,
+                    responseContainer = "List"),
+            @ApiResponse(code = 400, message = "Incomplete or inconsistent Request"),
+            @ApiResponse(code = 500, message = "Unknown technical error occurred.") })
+
+    @RequestMapping(method = RequestMethod.GET, value = "/")
+    public ResponseEntity<List<Result>> getProcesses() {
+
+        List<ProcessDefinition> list = engine.getRepositoryService()
+                .createProcessDefinitionQuery()
+                .active()
+                .latestVersion()
+                .list();
+
+        List<Result> results = new LinkedList<>();
+        list.forEach(element -> results.add(new Result(element.getId(), element.getName(), element.getKey())));
+
+        return ResponseEntity.ok(results);
+    }
+
+    @JsonPropertyOrder(alphabetic = true)
+    private class Result {
+        @JsonProperty
+        String processId;
+        @JsonProperty
+        String processName;
+        @JsonProperty
+        String processKey;
+
+        public Result(String processId, String processName, String processKey) {
+            this.processId = processId;
+            this.processKey = processKey;
+            this.processName = processName;
+        }
     }
 
 }
