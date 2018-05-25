48676970992bafdec9f08866ecef00e5c239db8a
Jasper Boyn <jasper.boyn@googlemail.com>
2018-05-25 12:37:54 +0200

    added arachni-process

---
+++ scb-scanprocesses/arachni-process/src/test/java/io/securecodebox/scanprocesses/AppTest.java
@@ -0,0 +1,20 @@
+package io.securecodebox.scanprocesses;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
