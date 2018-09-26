1f102d93da4ed236572e97e4210b7505842997ad
Jannik Hollenbach <Jannik.Hollenbach@iteratec.de>
2018-09-26 16:49:37 +0200

    Added new resource to start multiple different security tests

---
+++ scb-engine/src/main/java/io/securecodebox/engine/rest/SecurityTestRessource.java
@@ -0,0 +1,111 @@
+/*
+ *
+ *  SecureCodeBox (SCB)
+ *  Copyright 2015-2018 iteratec GmbH
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * /
+ */
+package io.securecodebox.engine.rest;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.securecodebox.constants.DefaultFields;
+import io.securecodebox.model.rest.SecurityTest;
+import io.securecodebox.scanprocess.ProcessVariableHelper;
+import io.swagger.annotations.*;
+import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.*;
+
+@Api(description = "Scan Tests Resource", produces = "application/json", consumes = "application/json")
+@RestController
+@RequestMapping(value = "/box/security-tests")
+public class SecurityTestRessource {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SecurityTestRessource.class);
+
+    @Autowired
+    ProcessEngine engine;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @ApiOperation(value = "Creates a new scan tests.")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    code = 201,
+                    message = "Successful created a new process returns the process id.",
+                    response = UUID.class,
+                    responseContainer = "List"
+            ),
+            @ApiResponse(
+                    code = 300,
+                    message = "For some reason multiple processes could be addressed by the given processKey.",
+                    response = void.class
+            ),
+            @ApiResponse(
+                    code = 400,
+                    message = "Incomplete or inconsistent Request"
+            ),
+            @ApiResponse(
+                    code = 404,
+                    message = "Could not find definition for specified security test.",
+                    response = void.class
+            ),
+            @ApiResponse(
+                    code = 500,
+                    message = "Unknown technical error occurred."
+            )
+    })
+    @RequestMapping(method = RequestMethod.PUT)
+    public ResponseEntity<List<UUID>> startSecurityTests(@Valid @RequestBody List<SecurityTest> securityTests) {
+
+        for(SecurityTest securityTest : securityTests){
+            long processCount = engine.getRepositoryService()
+                    .createProcessDefinitionQuery()
+                    .active()
+                    .processDefinitionKey(securityTest.getProcessDefinitionKey())
+                    .latestVersion()
+                    .count();
+
+            if(processCount == 0) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+            } else if(processCount > 1){
+                return ResponseEntity.status(HttpStatus.MULTIPLE_CHOICES).build();
+            }
+        }
+
+        List<UUID> processInstances = new LinkedList<>();
+
+        for (SecurityTest securityTest: securityTests) {
+            Map<String, Object> values = new HashMap<>();
+
+            values.put(DefaultFields.PROCESS_AUTOMATED.name(), true);
+            values.put(DefaultFields.PROCESS_CONTEXT.name(), securityTest.getContext());
+            values.put(DefaultFields.PROCESS_TARGETS.name(), ProcessVariableHelper.generateObjectValue(securityTest.getTarget()));
+
+            ProcessInstance instance = engine.getRuntimeService().startProcessInstanceByKey(securityTest.getProcessDefinitionKey(), values);
+            processInstances.add(UUID.fromString(instance.getProcessInstanceId()));
+        }
+
+        return ResponseEntity.ok(processInstances);
+    }
+}
---
+++ scb-sdk/src/main/java/io/securecodebox/model/rest/SecurityTest.java
@@ -0,0 +1,69 @@
+/*
+ *
+ *  SecureCodeBox (SCB)
+ *  Copyright 2015-2018 iteratec GmbH
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * /
+ */
+package io.securecodebox.model.rest;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.securecodebox.model.execution.Target;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(description = "A security scan contains the description of a target and the description of the method used to test the target for security defects.")
+public class SecurityTest{
+    @JsonProperty
+    @ApiModelProperty("Context references the larger scope the security test. In most cases this is equal to the name of the project.")
+    String context;
+
+    @JsonProperty("securitytest")
+    @ApiModelProperty("Security test to perform on the target.")
+    String securityTest;
+
+    @JsonProperty
+    @ApiModelProperty("The target of the security test.")
+    Target target;
+
+    public String getContext() {
+        return context;
+    }
+
+    public void setContext(String context) {
+        this.context = context;
+    }
+
+    public String getSecurityTest() {
+        return securityTest;
+    }
+
+    public void setSecurityTest(String securityTest) {
+        this.securityTest = securityTest;
+    }
+
+    public Target getTarget() {
+        return target;
+    }
+
+    public void setTarget(Target target) {
+        this.target = target;
+    }
+
+    @JsonIgnore
+    public String getProcessDefinitionKey(){
+        return this.getSecurityTest() + "-process";
+    }
+}
