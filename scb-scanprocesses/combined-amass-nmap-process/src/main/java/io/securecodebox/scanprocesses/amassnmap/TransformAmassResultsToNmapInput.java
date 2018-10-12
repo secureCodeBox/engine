0bc0a6c486c0b7f1b6d304de04f6bd82207a70a0
Martin Lang <Martin.Lang@iteratec.de>
2018-10-12 17:44:12 +0200

    Started implemening process model for amass-nmap scan

---
+++ scb-scanprocesses/combined-amass-nmap-process/src/main/java/io/securecodebox/scanprocesses/amassnmap/TransformAmassResultsToNmapInput.java
@@ -0,0 +1,73 @@
+package io.securecodebox.scanprocesses.amassnmap;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.securecodebox.constants.DefaultFields;
+import io.securecodebox.model.execution.Target;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.camunda.bpm.engine.variable.Variables;
+import org.camunda.bpm.engine.variable.value.ObjectValue;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import sun.rmi.runtime.Log;
+
+@Component
+public class TransformAmassResultsToNmapInput implements JavaDelegate {
+
+    private static final Logger LOG = LoggerFactory.getLogger(TransformAmassResultsToNmapInput.class);
+
+    @Override
+    public void execute(DelegateExecution execution) throws Exception {
+
+        LOG.info("----------------------------------------");
+        LOG.info("Trying to convert amass output to nmap input");
+        LOG.info("----------------------------------------");
+
+
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            String findingsAsString = objectMapper.writeValueAsString(execution.getVariable(DefaultFields.PROCESS_FINDINGS.name()));
+
+            List<Target> newTargets = objectMapper.readValue(objectMapper.readValue(findingsAsString, String.class),
+                    objectMapper.getTypeFactory().constructCollectionType(List.class, Target.class));
+
+            for (Target target : newTargets) {
+                target.getAttributes().put("hostname", target.getLocation());
+                target.setName("My Name Dummy");
+                // remove target configs
+            }
+
+            LOG.info("Created Targets out of Findings: " + newTargets);
+
+            ObjectValue objectValue = Variables.objectValue(objectMapper.writeValueAsString(newTargets))
+                    .serializationDataFormat(Variables.SerializationDataFormats.JSON)
+                    .create();
+            execution.setVariable(DefaultFields.PROCESS_TARGETS.name(), objectValue);
+
+            // SET NMAP PROCESS VARIABLES
+            execution.setVariable("NMAP_CONFIGURATION_TYPE","default");
+
+            LOG.info("FINISHED TransformAmassResultsToNmapInput Service Task. -> Start nmap");
+
+        } catch (JsonProcessingException e) {
+            throw new IllegalStateException("Can't write field to process!", e);
+        }
+
+
+
+    }
+
+//    private String removeProtocollFromUrl(String url) throws URISyntaxException {
+//        LOG.info("URL:" + url);
+//        URI uri = new URI(url);
+//        //TODO: not correct yet
+//        String path = uri.getHost() + uri.getPath(); // split whatever you need
+//        LOG.info("PATH:" + path);
+//        return path;
+//    }
+}
