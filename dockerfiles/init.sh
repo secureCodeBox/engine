67f7ab80532c83b9c512fccac89d76c06addaa7d
Robert Seedorff <Robert.Seedorff@iteratec.de>
2018-11-06 10:56:07 +0100

    Some minor refactorings regarding the OU naming

--- dockerfiles/init.sh
+++ dockerfiles/init.sh
@@ -6,7 +6,7 @@ create_self_signed_certificate()
 {
     echo "Creating self signed certificate..."
     keytool -genkey -alias scb-engine -storetype PKCS12 -keyalg RSA -keysize 2048 -keystore keystore.p12 -validity 3650 \
-    -dname "CN=SecureCodeboxEngine, OU=SecureCodebox, O=SecureCodebox.io, C=DE, ST=HH, L=Hamburg" \
+    -dname "CN=secureCodeBoxEngine, OU=secureCodeBox.io, O=secureCodeBox.io, C=DE, ST=HH, L=Hamburg" \
     -storepass "${SERVER_SSL_KEY_STORE_PASSWORD}"
 }
 
@@ -23,14 +23,14 @@ create_certificate_if_not_available()
 }
 
 echo "Execute init script:"
-echo "Check if https is enabled"
+echo "Check if HTTPS is enabled..."
 if [ "${SERVER_SSL_ENABLED}" == "true" ]
 then 
     echo "Https enabled"
     create_certificate_if_not_available
 else
-    echo "No https enabled"
+    echo "No HTTPS enabled. You can use environment variables to enable HTTPS."
 fi
 
-echo "Starting scb engine..."
+echo "Starting secureCodeBox engine..."
 java -Dloader.path="./lib/,./plugins/" -jar ./app.jar
