f28264f0ae1ea7d4a6b17ecdcd164a51bd460ca8
Jannik Hollenbach <Jannik.Hollenbach@iteratec.de>
2018-11-09 17:29:13 +0100

    Migrated s3 persistence provider to new PersistenceProvider interface

--- scb-persistenceproviders/s3-persistenceprovider/src/test/java/io/securecodebox/persistence/s3/S3PersistenceProviderTest.java
+++ scb-persistenceproviders/s3-persistenceprovider/src/test/java/io/securecodebox/persistence/s3/S3PersistenceProviderTest.java
@@ -19,13 +19,10 @@
 
 package io.securecodebox.persistence.s3;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import io.securecodebox.model.Report;
-import io.securecodebox.model.execution.ScanProcessExecution;
-import io.securecodebox.model.execution.Scanner;
-import io.securecodebox.model.execution.Target;
 import io.securecodebox.model.findings.Finding;
 import io.securecodebox.model.findings.Severity;
+import io.securecodebox.model.rest.Report;
+import io.securecodebox.model.securitytest.SecurityTest;
 import org.apache.commons.io.FileUtils;
 import org.junit.Before;
 import org.junit.Test;
@@ -40,106 +37,14 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 import static junit.framework.TestCase.assertEquals;
 
 @RunWith(MockitoJUnitRunner.class)
 public class S3PersistenceProviderTest {
-
-    ScanProcessExecution execution = new ScanProcessExecution() {
-        @Override
-        public UUID getId() {
-            return UUID.fromString("23701e7b-c9ec-46c9-aae8-57f5520f6c6c");
-        }
-
-        @Override
-        public void setContext(String id) {
-        }
-
-        @Override
-        public String getContext() {
-            return "KEKSE!!";
-        }
-
-        @Override
-        public boolean isRunning() {
-            return false;
-        }
-
-        @Override
-        public boolean hasScanner() {
-            return false;
-        }
-
-        @Override
-        public void addScanner(Scanner scanner) {
-        }
-
-        @Override
-        public List<Scanner> getScanners() {
-            return Collections.emptyList();
-        }
-
-        @Override
-        public List<Finding> getFindings() {
-            Finding finding = new Finding();
-            finding.setId(UUID.fromString("49bf7fd3-8512-4d73-a28f-608e493cd726"));
-            finding.setDescription("BAD_TEST_FINDIG_DESC");
-            finding.setName("BAD_TEST_FINDIG");
-            finding.setSeverity(Severity.HIGH);
-            findings.add(finding);
-            ;
-            return findings;
-        }
-
-        @Override
-        public String getRawFindings() {
-            return "";
-        }
-
-        @Override
-        public void clearFindings() {
-        }
-
-        @Override
-        public void appendFinding(Finding finding) {
-        }
-
-        @Override
-        public void appendTarget(Target target) {
-        }
-
-        @Override
-        public List<Target> getTargets() {
-            return Collections.emptyList();
-        }
-
-        @Override
-        public void clearTargets() {
-        }
-
-        @Override
-        public boolean isAutomated() {
-            return false;
-        }
-
-        @Override
-        public String getScannerType() {
-            return "TEST";
-        }
-    };
-
-    @Spy
-    ObjectMapper mapper = new ObjectMapper();
-
     @InjectMocks
     S3PersistenceProvider s3PersistenceProvider;
-
-    List<Finding> findings = new ArrayList<>();
 
     @Before
     public void setUp() {
@@ -148,14 +53,26 @@ public class S3PersistenceProviderTest {
 
     @Test
     public void testWriteReportToFile() throws IOException {
-        Report report = new Report(execution);
-        report.setId(UUID.fromString("281ccc0f-a933-4106-a3d3-209954e6305e"));
+        Finding finding = new Finding();
+        finding.setId(UUID.fromString("49bf7fd3-8512-4d73-a28f-608e493cd726"));
+        finding.setDescription("BAD_TEST_FINDIG_DESC");
+        finding.setName("BAD_TEST_FINDIG");
+        finding.setSeverity(Severity.HIGH);
+
+        List<Finding> findings = new LinkedList<>();
+        findings.add(finding);
+
+        Report report = new Report(findings, "<rawFindings/>");
+        report.setId(UUID.fromString("49bf7fd3-8512-4d73-a28f-608e493cd781"));
+
+        SecurityTest securityTest = new SecurityTest(UUID.fromString("281ccc0f-a933-4106-a3d3-209954e6305e"), "testContext","nmap", null, report);
         
-        File file = s3PersistenceProvider.writeReportToFile(report);
+        File file = s3PersistenceProvider.writeReportToFile(securityTest);
         String content = FileUtils.readFileToString(file, "UTF-8");
         assertEquals(
-                "{\"execution\":{\"id\":\"23701e7b-c9ec-46c9-aae8-57f5520f6c6c\",\"context\":\"KEKSE!!\",\"automated\":false,\"scanner_type\":\"TEST\",\"findings\":[{\"id\":\"49bf7fd3-8512-4d73-a28f-608e493cd726\",\"name\":\"BAD_TEST_FINDIG\",\"description\":\"BAD_TEST_FINDIG_DESC\",\"severity\":\"HIGH\",\"false_positive\":false}]},\"findings\":[{\"id\":\"49bf7fd3-8512-4d73-a28f-608e493cd726\",\"name\":\"BAD_TEST_FINDIG\",\"description\":\"BAD_TEST_FINDIG_DESC\",\"severity\":\"HIGH\",\"false_positive\":false},{\"id\":\"49bf7fd3-8512-4d73-a28f-608e493cd726\",\"name\":\"BAD_TEST_FINDIG\",\"description\":\"BAD_TEST_FINDIG_DESC\",\"severity\":\"HIGH\",\"false_positive\":false}],\"severity_highest\":\"HIGH\",\"severity_overview\":{\"HIGH\":4},\"report_id\":\"281ccc0f-a933-4106-a3d3-209954e6305e\"}",
-                content);
+            "{\"context\":\"testContext\",\"target\":null,\"id\":\"281ccc0f-a933-4106-a3d3-209954e6305e\",\"report\":{\"report_id\":\"49bf7fd3-8512-4d73-a28f-608e493cd781\",\"findings\":[{\"id\":\"49bf7fd3-8512-4d73-a28f-608e493cd726\",\"name\":\"BAD_TEST_FINDIG\",\"description\":\"BAD_TEST_FINDIG_DESC\",\"severity\":\"HIGH\",\"false_positive\":false}],\"raw_findings\":\"<rawFindings/>\",\"severity_highest\":\"HIGH\",\"severity_overview\":{\"HIGH\":1}},\"name\":\"nmap\",\"finished\":true}",
+            content
+        );
     }
 
     @Test
