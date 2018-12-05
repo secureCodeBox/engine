7ebcc198fdfabd0884d3238247539f6cfd77bec6
Jannik Hollenbach <Jannik.Hollenbach@iteratec.de>
2018-12-05 17:33:05 +0100

    Defect Dojo now also exports findings via raw defectdojo import

--- scb-persistenceproviders/defectdojo-persistenceprovider/src/main/java/io/securecodebox/persistence/DefectDojoPersistenceProvider.java
+++ scb-persistenceproviders/defectdojo-persistenceprovider/src/main/java/io/securecodebox/persistence/DefectDojoPersistenceProvider.java
@@ -18,36 +18,62 @@
  */
 package io.securecodebox.persistence;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.securecodebox.model.securitytest.SecurityTest;
 
 import io.securecodebox.persistence.models.EngagementPayload;
 import io.securecodebox.persistence.models.EngagementResponse;
+import io.securecodebox.persistence.models.ImportScanResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.*;
+import org.springframework.http.converter.FormHttpMessageConverter;
+import org.springframework.http.converter.ResourceHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+import java.util.List;
 
 @Component
 @ConditionalOnProperty(name = "securecodebox.persistence.provider", havingValue = "defectdojo")
-public class DefectDojoPersistenceProvider implements PersistenceProvider{
+public class DefectDojoPersistenceProvider implements PersistenceProvider {
     private static final Logger LOG = LoggerFactory.getLogger(DefectDojoPersistenceProvider.class);
 
     @Override
     public void persist(SecurityTest securityTest) throws PersistenceException {
         LOG.debug("Starting defectdojo persistence provider");
 
+        LOG.debug("RawFindings: {}", securityTest.getReport().getRawFindings());
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
         // TODO: Add more details to engagement
         ResponseEntity<EngagementResponse> res = createEngagement(securityTest);
 
-        LOG.debug("Created engagement: '{}'", res.getBody().getUrl());
+        String engagementUrl = res.getBody().getUrl();
 
-        // TODO: Attach raw findings to engagement
-        // parse raw findings json string
+        LOG.debug("Created engagement: '{}'", engagementUrl);
+
+        try {
+            String rawRawFindings = objectMapper.readValue(securityTest.getReport().getRawFindings(), String.class);
+
+            List<String> rawResults = objectMapper.readValue(rawRawFindings,
+                    objectMapper.getTypeFactory().constructCollectionType(List.class, String.class));
+            for (String rawResult : rawResults) {
+                createFindings(securityTest, rawResult, engagementUrl);
+            }
+        } catch (IOException e) {
+            LOG.error("Could not deserialize rawResults");
+            e.printStackTrace();
+        }
     }
 
     private ResponseEntity<EngagementResponse> createEngagement(SecurityTest securityTest) {
@@ -73,6 +99,56 @@ public class DefectDojoPersistenceProvider implements PersistenceProvider{
 
         HttpEntity<EngagementPayload> payload = new HttpEntity<>(engagementPayload, headers);
 
-        return restTemplate.exchange("http://localhost:8000/api/v2/engagements/", HttpMethod.POST,payload, EngagementResponse.class);
+        return restTemplate.exchange("http://localhost:8000/api/v2/engagements/", HttpMethod.POST, payload, EngagementResponse.class);
+    }
+
+    private ResponseEntity<ImportScanResponse> createFindings(SecurityTest securityTest, String rawResult, String engagementUrl) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        // TODO: Read Token from properties
+        headers.set("Authorization", "Token 27aceaf759994177c319a767c55b70f15ecb6b75");
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+        restTemplate.setMessageConverters(Arrays.asList(new FormHttpMessageConverter(), new ResourceHttpMessageConverter(), new MappingJackson2HttpMessageConverter()));
+
+        MultiValueMap<String, Object> mvn = new LinkedMultiValueMap<>();
+        mvn.add("engagement", engagementUrl);
+        // TODO: Fix User hardcoding
+        mvn.add("lead", "http://localhost:8000/api/v2/users/1/");
+        mvn.add("scan_date", "2018-12-04");
+        mvn.add("scan_type", getDefectDojoScanName(securityTest.getName()));
+
+        try {
+            ByteArrayResource contentsAsResource = new ByteArrayResource(rawResult.getBytes("UTF-8")) {
+                @Override
+                public String getFilename() {
+                    return "this_needs_to_be_here_but_doesnt_really_matter.txt";
+                }
+            };
+
+            mvn.add("file", contentsAsResource);
+
+            HttpEntity<MultiValueMap> payload = new HttpEntity<>(mvn, headers);
+
+            return restTemplate.exchange("http://localhost:8000/api/v2/import-scan/", HttpMethod.POST, payload, ImportScanResponse.class);
+        } catch (UnsupportedEncodingException e) {
+            LOG.error("UnsupportedEncodingException {}", e);
+        }
+        return null;
+    }
+
+    protected String getDefectDojoScanName(String securityTestName){
+        switch (securityTestName){
+            case "nmap":
+                return "Nmap Scan";
+            case "nikto":
+                return "Nikto Scan";
+            case "arachni":
+                return "Arachni Scan";
+            case "zap":
+                return "Zap Scan";
+            default:
+                throw new RuntimeException("No defectdojo parser for securityTest: '" + securityTestName + "'");
+        }
     }
 }
