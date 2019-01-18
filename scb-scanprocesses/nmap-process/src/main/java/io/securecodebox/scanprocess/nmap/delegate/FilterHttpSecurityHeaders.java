f4e2bd4e3b4bd03a41cb456b44b434aaa8838ee8
Jöran Tesse <joeran.tesse@iteratec.de>
2019-01-18 16:32:01 +0100

    work in progress

---
+++ scb-scanprocesses/nmap-process/src/main/java/io/securecodebox/scanprocess/nmap/delegate/FilterHttpSecurityHeaders.java
@@ -0,0 +1,147 @@
+package io.securecodebox.scanprocess.nmap.delegate;
+
+import io.securecodebox.model.execution.ScanProcessExecution;
+import io.securecodebox.model.execution.ScanProcessExecutionFactory;
+import io.securecodebox.model.findings.Finding;
+import io.securecodebox.model.findings.Severity;
+import io.securecodebox.scanprocess.nmap.util.HttpHeaderStrategy;
+import io.securecodebox.scanprocess.nmap.util.HttpHeaders;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+
+@Component
+public class FilterHttpSecurityHeaders implements JavaDelegate {
+
+    private static final Logger LOG = LoggerFactory.getLogger(FilterHttpSecurityHeaders.class);
+
+    @Autowired
+    ScanProcessExecutionFactory processExecutionFactory;
+
+
+    private static final HttpHeaderStrategy[] httpStrategies = new HttpHeaderStrategy[] {
+            requireHttpServersToRedirectToHttps()
+    };
+
+    private static final HttpHeaderStrategy[] httpsStrategies = new HttpHeaderStrategy[] {
+            requireStrictTransportSecurityHeader(),
+            requireContentSecurityPolicyHeader(),
+            requirePermittedCrossDomainPolicies(),
+            requireFrameOptionsToBeDenied(),
+            requireXssProtectionToBeEnabled(),
+            requireContentTypeOptionsToEqualNosniff()
+    };
+
+
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+        final ScanProcessExecution process = processExecutionFactory.get(delegateExecution);
+        final ArrayList<Finding> findings = new ArrayList<>();
+        final long T_START = System.currentTimeMillis();
+        process.getFindings().stream()
+                .filter(finding -> HttpHeaders.headersPresentInFinding(finding))
+                .forEach(finding -> {
+                    final HttpHeaders headers = HttpHeaders.fromFinding(finding);
+                    findings.addAll(applyStrategies (headers, finding));
+                });
+        final long T_STRATEGIES_APPLIED = System.currentTimeMillis();
+        final int numberOfAdditionalFindings = findings.size() - process.getFindings().size();
+        clearFindings(process);
+        findings.forEach(changedFinding -> process.appendFinding(changedFinding));
+        LOG.info("http-headers strategies yielded {} additional findings; finding them took {}ms, storing them {}ms", numberOfAdditionalFindings, T_STRATEGIES_APPLIED - T_START, System.currentTimeMillis() - T_STRATEGIES_APPLIED);
+    }
+
+    private ArrayList<Finding> applyStrategies(HttpHeaders headers, Finding finding) {
+        final ArrayList<Finding> changedFindings = new ArrayList<>();
+        changedFindings.add(finding);
+
+        final String name = finding.getName();
+
+        if (name.equals("http")) {
+            for (HttpHeaderStrategy httpStrategy : httpStrategies) {
+                changedFindings.addAll(httpStrategy.apply(headers, finding));
+            }
+        } else if (name.equals("https")) {
+            for (HttpHeaderStrategy httpsStrategy : httpsStrategies) {
+                changedFindings.addAll(httpsStrategy.apply(headers, finding));
+            }
+        }
+
+        return changedFindings;
+    }
+
+
+    private static HttpHeaderStrategy requireHttpServersToRedirectToHttps() {
+        return new HttpHeaderStrategy("Location")
+                .ifPresent()
+                .modifyFinding((value, finding) -> {
+                    finding.setSeverity(Severity.INFORMATIONAL);
+                    finding.setName(finding.getName() + " with redirect to " + value);
+                    finding.setDescription(finding.getDescription() + " with redirect to " + value);
+                })
+                .ifMissing()
+                .createFinding(Severity.HIGH, "missing redirect to https");
+    }
+
+    private static HttpHeaderStrategy requireStrictTransportSecurityHeader() {
+        return new HttpHeaderStrategy("Strict-Transport-Security")
+                .ifMissingCreateFinding(Severity.MEDIUM);
+    }
+
+    private static HttpHeaderStrategy requireContentSecurityPolicyHeader() {
+        return new HttpHeaderStrategy("Content-Security-Policy")
+                .ifMissingCreateFinding(Severity.MEDIUM);
+    }
+
+    private static HttpHeaderStrategy requirePermittedCrossDomainPolicies() {
+        return new HttpHeaderStrategy("X-Permitted-Cross-Domain-Policies")
+                .ifMissingCreateFinding(Severity.MEDIUM);
+    }
+
+    private static HttpHeaderStrategy requireFrameOptionsToBeDenied() {
+        return new HttpHeaderStrategy("X-Frame-Options")
+                .ifMissing()
+                .createFinding(Severity.MEDIUM, "X-Frame-Options header missing")
+                .ifTrue(value -> !value.toLowerCase().contains("deny"))
+                .createFinding(Severity.MEDIUM, "X-Frame-Options header misconfigured", value -> "X-Frame-Options should be 'deny', instead it is set to '" + value + "'");
+    }
+
+    private static HttpHeaderStrategy requireXssProtectionToBeEnabled() {
+        return new HttpHeaderStrategy("X-XSS-Protection")
+                .ifMissing()
+                .createFinding(Severity.MEDIUM, "X-XSS-Protection header missing")
+                .ifTrue(value -> value.startsWith("0"))
+                .createFinding(Severity.MEDIUM, "X-XSS-Protection manually disabled");
+    }
+
+    private static HttpHeaderStrategy requireContentTypeOptionsToEqualNosniff() {
+        return new HttpHeaderStrategy("X-Content-Type-Options")
+                .ifMissing()
+                .createFinding(Severity.MEDIUM, "X-Content-Type-Options header missing")
+                .ifTrue(value -> !value.equalsIgnoreCase("nosniff"))
+                .createFinding(Severity.MEDIUM, "X-Content-Type-Options misconfigured", value -> "X-Conntent-Type-Options should be set to 'nosniff' instead of '" + value + "'");
+    }
+
+
+    private void clearFindings(ScanProcessExecution process) {
+        if (!process.getFindings().isEmpty()) {
+            LOG.debug("Clearing findings. The process had {}", process.getFindings().size());
+            process.clearFindings();
+        }
+    }
+
+
+    private static final HttpHeaderStrategy requireHeader (final String header, final Severity severity) {
+        return new HttpHeaderStrategy(header)
+                .ifMissing()
+                .createFinding(severity, header + " header missing");
+    }
+
+}
---
+++ scb-scanprocesses/nmap-process/src/main/java/io/securecodebox/scanprocess/nmap/util/HttpHeaders.java
@@ -0,0 +1,53 @@
+package io.securecodebox.scanprocess.nmap.util;
+
+import io.securecodebox.model.findings.Finding;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public final class HttpHeaders {
+
+    private final HashMap<String, String> headers = new HashMap<>();
+
+    public HttpHeaders(String rawHeaders) {
+        final String[] lines = rawHeaders.split ("\\r?\\n");
+        for (int i = 0; i < lines.length; i++) {
+            final String line = lines[i];
+
+            // double-newline = end of http header section (ignore if first line; nmap bug)
+            if (line.length() == 0) {
+                if (i == 0) continue;
+                else break;
+            }
+
+            int colonPos = line.indexOf(':');
+            if (colonPos < 0) continue;
+            final String
+                    k = line.substring(0, colonPos).trim(),
+                    v = line.substring(colonPos + 1).trim();
+            headers.put(k, v);
+        }
+    }
+
+    public boolean has (String key) {
+        return headers.containsKey(key);
+    }
+
+    public String get (String key) {
+        return headers.get(key);
+    }
+
+    public static boolean headersPresentInFinding (Finding finding) {
+        Map<String, Object> attributes = finding.getAttributes();
+        if (attributes == null) return false;
+        Object scripts = attributes.get("scripts");
+        if (scripts == null || !(scripts instanceof Map)) return false;
+        return ((Map)scripts).containsKey("http-headers");
+    }
+
+    public static HttpHeaders fromFinding (Finding finding) {
+        if (!headersPresentInFinding(finding)) return null;
+        return new HttpHeaders(((Map<String,String>)finding.getAttributes().get("scripts")).get("http-headers"));
+    }
+
+}
