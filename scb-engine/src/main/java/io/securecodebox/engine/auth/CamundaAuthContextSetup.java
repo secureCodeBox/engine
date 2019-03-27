1b6e9e8b2f5be53a2afaf386d3568c9b0e5fd50b
Jannik Hollenbach <Jannik.Hollenbach@iteratec.de>
2019-03-27 12:54:39 +0100

    Added Camunda auth context filter

---
+++ scb-engine/src/main/java/io/securecodebox/engine/auth/CamundaAuthContextSetup.java
@@ -0,0 +1,73 @@
+/*
+ *
+ *  SecureCodeBox (SCB)
+ *  Copyright 2015-2018 iteratec GmbH
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * /
+ */
+package io.securecodebox.engine.auth;
+
+import io.securecodebox.engine.service.AuthService;
+import org.camunda.bpm.engine.IdentityService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+
+@Configuration
+@EnableWebSecurity
+@Order(1231231)
+public class CamundaAuthContextSetup extends WebSecurityConfigurerAdapter {
+    private static final Logger LOG = LoggerFactory.getLogger(CamundaAuthContextSetup.class);
+
+    @Autowired
+    IdentityService identityService;
+
+    @Autowired
+    private AuthService authService;
+
+    @Override
+    public void configure(HttpSecurity http) {
+        LOG.debug("Init: Camunda Auth Contex Setup Filter");
+        http.antMatcher("/box/**").addFilterAfter(new CamundaAuthContextSetupFilter(), FilterSecurityInterceptor.class);
+    }
+
+    /**
+     * Sets up the Camunda Authentication Context before the Resource gets executed
+     */
+    private class CamundaAuthContextSetupFilter extends GenericFilterBean {
+        @Override
+        public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+            LOG.debug("Setting up Camunda Auth Context for request");
+
+            identityService.clearAuthentication();
+
+            identityService.setAuthentication(authService.getAuthentication());
+
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
+    }
+}
