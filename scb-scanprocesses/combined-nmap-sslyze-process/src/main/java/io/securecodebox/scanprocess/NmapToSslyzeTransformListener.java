6ba8e164c58448ffc06a59b78f4a71de252aa61f
dpatanin <daniel.patanin@iteratec.com>
2020-03-03 15:35:23 +0100

    WIP new combined process & fix combined processes

---
+++ scb-scanprocesses/combined-nmap-sslyze-process/src/main/java/io/securecodebox/scanprocess/NmapToSslyzeTransformListener.java
@@ -0,0 +1,56 @@
+package io.securecodebox.scanprocess;
+
+import io.securecodebox.constants.DefaultFields;
+import io.securecodebox.model.execution.Target;
+import io.securecodebox.model.findings.Finding;
+import io.securecodebox.scanprocess.listener.TransformFindingsToTargetsListener;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class NmapToSslyzeTransformListener extends TransformFindingsToTargetsListener {
+
+    public void notify(DelegateExecution delegateExecution) throws Exception {
+
+        List<Finding> findings = ProcessVariableHelper.readListFromValue(
+                (String) delegateExecution.getVariable(DefaultFields.PROCESS_FINDINGS.name()),
+                Finding.class
+        );
+
+        List<Target> newTargets = findings.stream()
+                .filter(finding -> finding.getCategory().equals("Open Port"))
+                .filter(finding -> {
+                    String service = (String) finding.getAttribute(OpenPortAttributes.service);
+                    return service.contains("https");
+                })
+                .map(finding -> {
+                    String hostname = (String) finding.getAttribute(OpenPortAttributes.hostname);
+                    String port = finding.getAttribute(OpenPortAttributes.port).toString();
+
+                    Target target = new Target();
+                    target.setName("SSLyze Scan for " + hostname);
+                    target.setLocation(hostname + ":" + port);
+
+                    return target;
+                }).collect(Collectors.toList());
+
+        LOG.info("Created Targets out of Findings: " + newTargets);
+
+        if (!newTargets.isEmpty() && newTargets.size() > 0) {
+            // define the new SSLyze targets, based on the nmap port scan results
+            delegateExecution.setVariable(DefaultFields.PROCESS_TARGETS.name(),
+                    ProcessVariableHelper.generateObjectValue(newTargets)
+            );
+        }
+        else {
+            // if no new target had been found clear the target parameter (and skip the SSLyze scan)
+            delegateExecution.setVariable(DefaultFields.PROCESS_TARGETS.name(),
+                    ""
+            );
+        }
+    }
+
+}
