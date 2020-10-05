01f1f2ebddebe930f8c7a8359fb206a743e02691
Samuel Graf <samuel.graf@iteratec.de>
2020-10-05 18:56:43 +0200

    move counting endpoint to box/securityTets/definitions

--- scb-engine/src/main/java/io/securecodebox/engine/rest/SecurityTestDefinitionResource.java
+++ scb-engine/src/main/java/io/securecodebox/engine/rest/SecurityTestDefinitionResource.java
@@ -23,14 +23,11 @@ import io.securecodebox.engine.model.PermissionType;
 import io.securecodebox.engine.model.ResourceType;
 import io.securecodebox.engine.service.AuthService;
 import io.securecodebox.engine.service.SecurityTestService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
-import io.swagger.annotations.Authorization;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,7 +36,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Api(value = "securityTestDefinitions",
-        description = "Lists available definitions of securityTests.",
+        description = "Retrieve information about the available securityTest definitions.",
         produces = "application/json",
         consumes = "application/json")
 @RestController
@@ -90,5 +87,61 @@ public class SecurityTestDefinitionResource {
         List<String> securityTests = securityTestService.getAvailableSecurityTestDefinitionNames();
 
         return ResponseEntity.ok(securityTests);
+    }
+
+    @ApiOperation(
+            value = "Count the active securityTests belonging to a fixed definition",
+            notes = "Only those security test instances are included in the counting which the requesting user is " +
+                    "allowed to see. " +
+                    "In the case that the there is no security test definition (process definition) with the name " +
+                    "that is provided as path parameter, the count is defined as zero.",
+            response = long.class,
+            authorizations = {
+                    @Authorization(value = "basicAuth")
+            }
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    code = 200,
+                    message = "Successful fetched the number of active security tests with the specified definition " +
+                            "name",
+                    response = long.class
+            ),
+            @ApiResponse(
+                    code = 401,
+                    message = "Unauthenticated",
+                    response = void.class
+            ),
+            @ApiResponse(
+                    code = 403,
+                    message = "Unauthorized, the user is missing the required rights to perform this action.",
+                    response = void.class
+            ),
+            @ApiResponse(
+                    code = 500,
+                    message = "Unknown technical error occurred."
+            )
+    })
+    @RequestMapping(method = RequestMethod.GET, value = "/{testDefinitionName}/countActiveTests")
+    public ResponseEntity<Long> getCountOfActiveSecurityTestsFilteredByDefinitionName(
+            @PathVariable
+            @ApiParam(
+                    value = "The number of active security test instances for which the definition has this name " +
+                            "shall be counted.",
+                    required = true
+            )
+                    String testDefinitionName
+    ) {
+        try {
+            authService.checkAuthorizedFor(ResourceType.SECURITY_TEST, PermissionType.READ);
+        } catch (InsufficientAuthorizationException e){
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+
+        long countOfActiveTestsFilteredByDefinitionName = securityTestService.countActiveTestsFilteredByDefinitionName(
+                testDefinitionName
+        );
+
+        return ResponseEntity.ok(countOfActiveTestsFilteredByDefinitionName);
     }
 }
